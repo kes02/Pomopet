0ebedf69c50744a2b1f7b75523095e862b71f1e6
Heejin Jung <hijin1220@gmail.com>
2026-06-22 11:39:23 +0900

    Initial Commit

---
+++ Pomopet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomopet
+//
+//  Created by Heejin Jung on 6/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomopet/PomopetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomopetApp.swift
+//  Pomopet
+//
+//  Created by Heejin Jung on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PomopetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
